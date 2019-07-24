1f2fd3150f604192bca905c44ce8c7516b4f59b0
lefeuvre <lefeuvre.kev@gmail.com>
2019-07-24 17:37:02 +0200

    add analogs inputs

--- esp32.ino
+++ esp32.ino
@@ -2,9 +2,6 @@
 #include "ESPAsyncWebServer.h"
 #include "SPIFFS.h"
 
-#define LOW   (uint8_t)0x00
-#define HIGH  (uint8_t)0x01
-
 typedef enum
 {
   LED_NONE    = (uint8_t)0,
@@ -241,7 +238,7 @@ void setup() {
 }
 
 void loop(){
-  uint8_t tmp[8];
+  uint8_t tmp[8] = {0};
   
   while (Serial.available()) {
     if (Serial.read() == 0x4F) {
@@ -249,43 +246,50 @@ void loop(){
         for (int i=0; i<8; i++) {
           tmp[i] = Serial.read();
         }
-        if (calculChecksum(tmp, 8) == ((Serial.read() << 8) + Serial.read())) {
-          if (tmp[0] == 0x35) { // PROCESS_GET_DATA
-            if (tmp[3] == 0x00 && tmp[7] == LOW) {
+        if (calculChecksum(tmp, 8) == ((Serial.read() << 8) | Serial.read())) {
+          // PROCESS_SEND_DATA  
+          if (tmp[0] == 0x36) {          
+            if (tmp[3] == 0x00 && tmp[7] == 0x00) {
               etor_state[0] = "LOW";
             }
-            else if (tmp[3] == 0x00 && tmp[7] == HIGH) {
+            else if (tmp[3] == 0x00 && tmp[7] == 0x01) {
               etor_state[0] = "HIGH";
             }
-            else if (tmp[3] == 0x01 && tmp[7] == LOW) {
+            else if (tmp[3] == 0x01 && tmp[7] == 0x00) {
               etor_state[1] = "LOW";
             }
-            else if (tmp[3] == 0x01 && tmp[7] == HIGH) {
+            else if (tmp[3] == 0x01 && tmp[7] == 0x01) {
               etor_state[1] = "HIGH";
             }
-            else if (tmp[3] == 0x02 && tmp[7] == LOW) {
+            else if (tmp[3] == 0x02 && tmp[7] == 0x00) {
               etor_state[2] = "LOW";
             }
-            else if (tmp[3] == 0x02 && tmp[7] == HIGH) {
+            else if (tmp[3] == 0x02 && tmp[7] == 0x01) {
               etor_state[2] = "HIGH";
             }
-            else if (tmp[3] == 0x03 && tmp[7] == LOW) {
+            else if (tmp[3] == 0x03 && tmp[7] == 0x00) {
               etor_state[3] = "LOW";
             }
-            else if (tmp[3] == 0x03 && tmp[7] == HIGH) {
+            else if (tmp[3] == 0x03 && tmp[7] == 0x01) {
               etor_state[3] = "HIGH";
             }
-            else if (tmp[3] == 0x04 && tmp[7] == LOW) {
+            else if (tmp[3] == 0x04 && tmp[7] == 0x00) {
               etor_state[4] = "LOW";
             }
-            else if (tmp[3] == 0x04 && tmp[7] == HIGH) {
+            else if (tmp[3] == 0x04 && tmp[7] == 0x01) {
               etor_state[4] = "HIGH";
             }
-            else if (tmp[3] == 0x05 && tmp[7] == LOW) {
+            else if (tmp[3] == 0x05 && tmp[7] == 0x00) {
               etor_state[5] = "LOW";
             }
-            else if (tmp[3] == 0x05 && tmp[7] == HIGH) {
+            else if (tmp[3] == 0x05 && tmp[7] == 0x01) {
               etor_state[5] = "HIGH";
+            }
+            else if (tmp[3] == 0x06) {
+              an_value[0] = String((tmp[4] << 24) | (tmp[5] << 16) | (tmp[6] << 8) | tmp[7]);
+            }
+            else if (tmp[3] == 0x07) {
+              an_value[1] = String((tmp[4] << 24) | (tmp[5] << 16) | (tmp[6] << 8) | tmp[7]);
             }
           }
         }
